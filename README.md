fd973a2aa5628a84810b10c2b6dd2e0f2d320a23
diegomuner <diegomuner@hotmail.com>
2023-03-16 04:12:43 -0300

    Added a Readme to help not forget commands and will keep adding to it as i find useful things to add

---
+++ README.md
@@ -0,0 +1,15 @@
+Project to Ramp Up Automation with Playwright
+
+
+Trello Board to Track progress:
+https://trello.com/b/XnPThDOn/playwright-project
+
+Private Repo:
+https://github.com/diegomuner/PlaywrightProject
+
+How to use:
+
+npx playwright test filename                     // this runs the spec file by name
+npx playwright test                              // this runs all spec files
+npx playwright codegen                           // this opens a tool that helps creating tests by writing the locators and actions for them to be copy pasted into your class
+npx playwright test filename --debug             // this will run the test in debug mode
--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -1,10 +1,41 @@
 import { test, expect } from '@playwright/test';
 
+test.beforeEach(async ({ page }, testInfo) => {
+    // Go to the starting url before each test.
 
-test('has title', async ({ page }) => {
+    if (testInfo.title != 'check google bro'){
     await page.goto('https://www.saucedemo.com/');
+    
+    }
+  });
 
 
+test('has title', async ({page}, testInfo) => {
+   
 // Expect a title "to contain" a substring.
 await expect(page).toHaveTitle(/Swag Labs/);
 });
+
+
+test('login using given credentials', async ({ page }, testInfo) => {
+
+    console.log(testInfo.title);
+  await page.goto('https://www.saucedemo.com/');
+  await expect(page).toHaveTitle(/Swag Labs/)
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+});
+
+test('check google bro', async ({ page }) => {
+
+    await page.goto('https://www.google.com/');
+    await expect(page.getByRole('img', { name: 'Google' })).toBeVisible();
+    // code below takes a snapshot and compares against base image. The 1st time it runs it fails as there is no base image yet
+//     expect(await page.screenshot()).toMatchSnapshot(); 
+    
+
+});
+
